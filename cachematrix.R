c0c6d27632db8a8d4179ceae24c3e204af150243
msarmet <msarmet@gmail.com>
2014-09-20 11:02:05 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## this function creates a the inverse of a matrix in the cache. It also allows us to retrieve it. 
+## Also, it creates an object that contains the inverse of this matrix.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){          # This will set m to NULL and set x to argument y
+      m <- NULL      
+      set <- function(y) {    
+            x <<- y
+            m <<- NULL
+      }
+      
+## The code below will define the vector of functions that allow us to return m (get), 
+## create the inverse of a matrix and assign to m (setinverted), and get the inverted matrix (getinverted)
+      
+      get <- function () x
+      setinverted <- function(solve) m <<- solve 
+      getinverted <- function() m 
+      list(set = set, get = get, setinverted = setinverted, getinverted = getinverted) 
 }
 
+## This function calculates the inverse of a matrix "x". If the
+## it has already been calculated and stored, 
+## the function returns the stored inverted matrix.
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){ # get the inverted matrix, considering if theres data in the cache or not.
+      m <- x$getinverted()
+      if(!is.null(m)) {     # if there is data in the cache, return m.
+            message("getting data from cache")
+            return(m)
+      }    # if there is no data in the cache, create the inverted matrix and use setinverted to chache it. 
+      data <- x$get()
+      m <- solve(data, ...)    
+      x$setinverted(m)     
+      m   # return solved matrix
 }
+
